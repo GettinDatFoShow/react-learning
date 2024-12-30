7e6c1a397fd60c6667c1649c2a9e98c1b5e5c1e0
Robert Morris <gettindatfoshow@gmail.com>
2024-12-30 15:40:39 -0500

    useEffect  example

--- ll-gift-card-lab/src/App.js
+++ ll-gift-card-lab/src/App.js
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-export default function App() {
+function GiftCard() {
   const [giftCard, setGiftCard] = useState(
     {
         firstName: "Jennifer",
@@ -16,26 +16,60 @@ export default function App() {
   }
 
   return (
+    <>
+        <h1>
+          Gift Card Page
+        </h1>
+        <h2>
+          Customer: {giftCard.firstName} {giftCard.lastName}
+        </h2>
+        <h3>
+          {giftCard.text}
+        </h3>
+        <p>
+          {giftCard.instructions}
+        </p>
+        {
+          giftCard.valid && (
+            <button onClick={spendGiftCard}>
+              Spend Gift Card
+            </button>
+          )
+        }
+    </>
+  )
+}
+
+function UseEffectExample()  {
+  const [toggle, setToggle] = useState(false);
+  const clickHandler = () => {
+    setToggle(!toggle);
+  }
+
+  useEffect(()=>{
+    document.title = toggle ? 'Welcome to Little Lemon' : 'Using the useEffect hook'
+  }, [toggle])
+
+  return (
+    <>
+      <h1>Using the useEffect hook</h1>
+      <button onClick={clickHandler}>
+        Toggle Message
+      </button>
+      {toggle && <h2>Welcom to Little Lemon</h2>}
+    </>
+  )
+}
+
+export default function App() {
+  return (
     <div style={{padding: '40px'}}>
-      <h1>
-        Gift Card Page
-      </h1>
-      <h2>
-        Customer: {giftCard.firstName} {giftCard.lastName}
-      </h2>
-      <h3>
-        {giftCard.text}
-      </h3>
-      <p>
-        {giftCard.instructions}
-      </p>
-      {
-        giftCard.valid && (
-          <button onClick={spendGiftCard}>
-            Spend Gift Card
-          </button>
-        )
-      }
+      <section>
+        <GiftCard />
+      </section>
+      <section>
+        <UseEffectExample />
+      </section>
     </div>
   );
 }
