0bdc36fd59bfdec0d5dd7f55d57956b979e3908d
Robert Morris <gettindatfoshow@gmail.com>
2024-12-30 13:45:45 -0500

    updated learning

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,22 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const little = "Little Lemon";
+  const big = "Big Lemon";
+  const [restaurantName, setRestaurantName] = useState(little)
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{restaurantName}</h1>
+        <button type="button" onClick={()=>setRestaurantName(big)}>If It's Small</button>
+        <button type="button" onClick={()=>setRestaurantName(little)}>If It's Big</button>
       </header>
     </div>
   );
