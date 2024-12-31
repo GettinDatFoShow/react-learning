32e9326f71c6f5667b650ad02144e07037f5bec0
Robert Morris <gettindatfoshow@gmail.com>
2024-12-31 15:55:54 -0500

    random fetch customer  data example  added

---
+++ random-user-fetch-example/src/App.js
@@ -0,0 +1,40 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [user, setUser] = useState([]);
+
+  const fetchUserData = () => {
+    fetch('https://randomuser.me/api/?results=1')
+    .then((response) => response.json())
+    .then((responseJson) => setUser(responseJson.results[0]))
+  }
+
+  useEffect(() => {
+    fetchUserData();
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+          { Object.keys(user).length > 0 ? (
+          <div>
+            <h1>Random Customer Data</h1>
+            <h2>{user.name.first} {user.name.last}</h2>
+            <img src={user.picture.medium} className="App-logo" alt="user-photo" />
+          </div>
+        ) : (
+          <>
+          <h1>Random Customer Data pending...</h1>
+          <img src={logo} className="App-logo" alt="app-logo" />
+          </>
+        )}
+
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
