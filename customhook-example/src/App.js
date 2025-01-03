bf80bc845ed16b2d1877812554c3f765cd30455b
Robert Morris <gettindatfoshow@gmail.com>
2025-01-03 15:25:15 -0500

    added custom hook example

--- customhook-example/src/App.js
+++ customhook-example/src/App.js
@@ -1,22 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import useConsoleLog from './useConsoleLog';
 
 function App() {
+  document.title = 'React Custom Hook Example';
+  const [count, setCount] = useState(0); 
+  useConsoleLog(count);
+  function increment() { 
+    setCount(prevCount => prevCount + 1) 
+  } 
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Count: {count}</h1> 
+        <button onClick={increment}>Plus 1</button> 
       </header>
     </div>
   );
